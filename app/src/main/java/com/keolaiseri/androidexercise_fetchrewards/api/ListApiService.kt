d4f8af24e4597940e49bb6656224e567ab0f7c8f
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2022-05-22 10:36:17 -1000

    updated interface in API service

--- app/src/main/java/com/keolaiseri/androidexercise_fetchrewards/api/ListApiService.kt
+++ app/src/main/java/com/keolaiseri/androidexercise_fetchrewards/api/ListApiService.kt
@@ -4,12 +4,22 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 private const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
 
 
-class FetchRewardsApiService {
+enum class ListFilter(val value: String)
+
+
+
+
+interface ListApiService{
+
+    @GET("items")
+    suspend fun getProperties(@Query("filter") type: String): List<ListItem>
 }
 
 private val moshi = Moshi.Builder()
@@ -20,3 +30,8 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
+
+
+object MarsApi {
+    val retrofitService : ListApiService by lazy { retrofit.create(ListApiService::class.java) }
+}
---
+++ app/src/main/java/com/keolaiseri/androidexercise_fetchrewards/api/ListItem.kt
@@ -0,0 +1,7 @@
+package com.keolaiseri.androidexercise_fetchrewards.api
+
+data class ListItem(
+    val id: Int,
+    val listId: Int,
+    val name: String
+)
